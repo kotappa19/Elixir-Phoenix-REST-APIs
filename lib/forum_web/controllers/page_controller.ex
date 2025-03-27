ea21856c87bcfabff68bf5d87b6c082e044086fb
kotappa19 <kgandudi@gmail.com>
2025-03-27 08:44:09 +0530

    Added REST APIs for posts

--- lib/forum_web/controllers/page_controller.ex
+++ lib/forum_web/controllers/page_controller.ex
@@ -6,4 +6,13 @@ defmodule ForumWeb.PageController do
     # so skip the default app layout.
     render(conn, :home, layout: false)
   end
+
+  def users(conn, _params) do
+    users = [
+      %{id: 1, name: "Arjun", email: "arjun@gmail.com"},
+      %{id: 2, name: "Bheem", email: "bheem@gmail.com"}
+
+    ]
+    json(conn, %{users: users})
+  end
 end
